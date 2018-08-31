23622c432a49e4d37c4f77911c0b7287b774971c
Elizabeth Rosso <elizabeth.a.rosso@gmail.com>
2018-08-31 17:19:59 +0000

    Text File

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System;
 // ﻿1- Write a program that reads a text file and displays the number of words.
 // 2- Write a program that reads a text file and displays the longest word in the file.
 
+//string[] StringArray = s.Split(' ');
+
 namespace Section9
 {
     class Program
@@ -15,25 +17,46 @@ namespace Section9
             string OldFile = @"/home/ubuntu/workspace/Section9/files/text.txt";
             string NewFile = @"/home/ubuntu/workspace/Section9/files/text.bak";
             
-            //CopyFiles(OldFile, NewFile);
+            CopyFile(OldFile, NewFile);
+            
+            int counter = 0;  
+            string line;  
 
-            StreamReader sr = new StreamReader(NewFile);
-                while (sr.ReadLine()!=null)
-            {
-                Line =sr.ReadLine();
-                Console.WriteLine(Line);
-            }
+            int CountWords = 0;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(@OldFile);  
+            while((line = file.ReadLine()) != null)  
+            {  
+                Console.WriteLine(String.Format("{0}",line));
+                string[] StringArray = line.Split(' '); 
+                CountWords += StringArray.Length;
+                
+                //System.Console.WriteLine(line);  
+                for(int i = 0; i < line.Length; i++)
+                {
+                    // do nothing
+                }    
+                
+                counter++;  
+            }  
+
+            file.Close();  
+            System.Console.WriteLine("There were {0} lines.", counter);  
+            System.Console.WriteLine("There were {0} words.", CountWords);  
+            // Suspend the screen.  
+            System.Console.ReadLine(); 
 
         }
         
-        static int CopyFiles(string Old, string New)
+        static int CopyFile(string Old, string New)
         {
             if(File.Exists(New))
                 File.Delete(New);
             
             File.Copy(Old, New);   
             
-            Console.WriteLine("You are good");            
+            //Console.WriteLine("You are good");            
             return 0;
         }
         
