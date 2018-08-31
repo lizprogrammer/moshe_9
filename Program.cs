5374ff4a15c21dbd6e416180593785d33a88b9ee
Elizabeth Rosso <elizabeth.a.rosso@gmail.com>
2018-08-31 21:43:01 +0000

    Text File

--- Program.cs
+++ Program.cs
@@ -18,34 +18,8 @@ namespace Section9
             string NewFile = @"/home/ubuntu/workspace/Section9/files/text.bak";
             
             CopyFile(OldFile, NewFile);
-            
-            int counter = 0;  
-            string line;  
-
-            int CountWords = 0;
-
-            // Read the file and display it line by line.  
-            System.IO.StreamReader file = new System.IO.StreamReader(@OldFile);  
-            while((line = file.ReadLine()) != null)  
-            {  
-                Console.WriteLine(String.Format("{0}",line));
-                string[] StringArray = line.Split(' '); 
-                CountWords += StringArray.Length;
-                
-                //System.Console.WriteLine(line);  
-                for(int i = 0; i < line.Length; i++)
-                {
-                    // do nothing
-                }    
-                
-                counter++;  
-            }  
-
-            file.Close();  
-            System.Console.WriteLine("There were {0} lines.", counter);  
-            System.Console.WriteLine("There were {0} words.", CountWords);  
-            // Suspend the screen.  
-            System.Console.ReadLine(); 
+            CountWords(OldFile);
+            Console.WriteLine(String.Format("The biggest word in the file is {0}", BiggestWord(OldFile)));
 
         }
         
@@ -60,9 +34,81 @@ namespace Section9
             return 0;
         }
         
+        static int CountWords(string FileName)
+        {
+            
+            int counter = 0;  
+            string line;  
 
+            int CountWords = 0;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(@FileName);  
+            while((line = file.ReadLine()) != null)  
+            {  
+                Console.WriteLine(String.Format("{0}",line));
+                string[] StringArray = line.Split(' '); 
+                CountWords += StringArray.Length;
+                
+                //System.Console.WriteLine(line);  
+                for(int i = 0; i < line.Length; i++)
+                {
+                    // do nothing
+                }    
+                counter++;  
+            }  
+
+            file.Close();  
+            System.Console.WriteLine("There are {0} lines.", counter);  
+            System.Console.WriteLine("There are {0} words.", CountWords);  
+            // Suspend the screen.  
+            System.Console.ReadLine(); 
+                
+            return 0;            
+        }
+        
+        static string BiggestWord(string MyFile)
+        {
+                      
+            int counter = 0;  
+            string line;  
+            string BiggestWord = "";
+            int BiggestSize = 0;            
+
+            int CountWords = 0;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(@MyFile);  
+            while((line = file.ReadLine()) != null)  
+            {  
+                
+                Console.WriteLine(String.Format("{0}",line));
+                string[] StringArray = line.Split(' '); 
+                CountWords += StringArray.Length;
+                
+                
+                //System.Console.WriteLine(line);  
+                foreach(string str in StringArray)
+                {
+                    if(str.Length > BiggestSize)
+                    {
+                        BiggestWord = str;
+                        BiggestSize = str.Length;
+                    }   
+                }
+                counter++;  
+            }  
+
+            file.Close();  
+            System.Console.WriteLine("There are {0} lines.", counter);  
+            System.Console.WriteLine("There are {0} words.", CountWords);  
+            // Suspend the screen.  
+            System.Console.ReadLine(); 
+            
+            
+            return BiggestWord;
+            
+        }
     }
     
-    
-
 }
